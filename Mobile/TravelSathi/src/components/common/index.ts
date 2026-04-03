c4d0b5c542b65e29359ea60be3c26b305e103e08
dineshyr29-04 <dineshyr2904@gmail.com>
2026-04-03 19:50:53 +0530

    the base files tsx was added

---
+++ Mobile/TravelSathi/src/components/common/index.ts
@@ -0,0 +1,7 @@
+/**
+ * Barrel Exports
+ * Export all common components from one location
+ */
+
+export { default as Button } from './Button';
+export { default as LoadingSpinner } from './LoadingSpinner';
