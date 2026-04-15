761e7a80fc944b9dff1b058ed3ae227805f130c2
dineshyr29-04 <dineshyr2904@gmail.com>
2026-04-15 21:59:26 +0530

    feat: Add initial layout and homepage structure with components for travel website
    - Create RootLayout component for global styles and metadata
    - Implement Home component with Navbar, Hero, DestinationCard, and Footer
    - Add DestinationCard component for displaying featured destinations
    - Create Footer component with links and newsletter subscription
    - Develop Hero component with background image and search functionality
    - Introduce Icons component for reusable SVG icons
    - Build Navbar component with responsive design and mobile menu
    - Add global CSS styles for theming and custom scrollbar
    - Configure TypeScript settings for the project

---
+++ Frontend-web/.next/dev/static/chunks/[project]_Travel-App_Frontend-web_src_pages__app_07xvfw~._.js
@@ -0,0 +1,5 @@
+(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: ["static/chunks/00c4_next_dist_compiled_09_5shy._.js","static/chunks/00c4_next_dist_shared_lib_0h0c-~q._.js","static/chunks/00c4_next_dist_client_0qh.3x_._.js","static/chunks/00c4_next_dist_11y4_i7._.js","static/chunks/04fx_next_app_0fd-mgq.js","static/chunks/[next]_entry_page-loader_ts_0m2l8rn._.js","static/chunks/00c4_react-dom_106ty6d._.js","static/chunks/00c4_0wp5qqk._.js","static/chunks/[root-of-the-server]__02l6h9q._.js"],
+    source: "entry"
+});
---
+++ Frontend-web/.next/dev/static/chunks/pages/_app.js
@@ -0,0 +1,13 @@
+__turbopack_load_page_chunks__("/_app", [
+  "static/chunks/00c4_next_dist_compiled_09_5shy._.js",
+  "static/chunks/00c4_next_dist_shared_lib_0h0c-~q._.js",
+  "static/chunks/00c4_next_dist_client_0qh.3x_._.js",
+  "static/chunks/00c4_next_dist_11y4_i7._.js",
+  "static/chunks/04fx_next_app_0fd-mgq.js",
+  "static/chunks/[next]_entry_page-loader_ts_0m2l8rn._.js",
+  "static/chunks/00c4_react-dom_106ty6d._.js",
+  "static/chunks/00c4_0wp5qqk._.js",
+  "static/chunks/[root-of-the-server]__02l6h9q._.js",
+  "static/chunks/[project]_Travel-App_Frontend-web_src_pages__app_07xvfw~._.js",
+  "static/chunks/turbopack-[project]_Travel-App_Frontend-web_src_pages__app_0iiw1.a._.js"
+])
